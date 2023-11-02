b6a7a783a35f6a754a54c937cb8ffc857f71e0ef
Eric Tobin <eatobin@gmail.com>
2023-11-02 17:40:00 -0500

    first commit

---
+++ lib/day01.exs
@@ -0,0 +1,26 @@
+defmodule Day01 do
+  def haiku() do
+    {:ok, contents} = File.read("day01.txt")
+
+    contents
+    |> String.split("\n", trim: true)
+    |> Enum.map(&String.to_integer/1)
+  end
+
+  def large() do
+    {:ok, csv} = File.read("day02.csv")
+
+    [_head | tail] =
+      csv
+      |> String.trim_trailing()
+      |> String.split(",")
+      |> Enum.map(&String.to_integer/1)
+
+    tail
+  end
+end
+
+# iex(3)> c "lib/day01.exs"
+# [Haiku]
+# iex(4)> Day01.haiku
+# iex(9)> r Day01
---
+++ test/advent19_elixir_test.exs
@@ -0,0 +1,8 @@
+defmodule Advent19ElixirTest do
+  use ExUnit.Case
+  doctest Advent19Elixir
+
+  test "greets the world" do
+    assert Advent19Elixir.hello() == :world
+  end
+end
