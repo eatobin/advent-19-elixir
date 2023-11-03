43f3d747ad54c159b73af4ace242b88fa1d02c2f
Eric Tobin <eatobin@gmail.com>
2023-11-02 22:32:18 -0500

    head

--- lib/day01.exs
+++ lib/day01.exs
@@ -10,13 +10,13 @@ defmodule Day01 do
   def large() do
     {:ok, csv} = File.read("day02.csv")
 
-    [_head | tail] =
+    [head | _tail] =
       csv
       |> String.trim_trailing()
       |> String.split(",")
       |> Enum.map(&String.to_integer/1)
 
-    tail
+    head
   end
 end
 
