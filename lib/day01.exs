5e6907c6dc14627377298f1f379ea460ed37c058
Eric Tobin <eatobin@gmail.com>
2023-11-03 16:00:56 -0500

    part a works

--- lib/day01.exs
+++ lib/day01.exs
@@ -1,24 +1,43 @@
 defmodule Day01 do
-  def haiku() do
-    {:ok, contents} = File.read("day01.txt")
+  def make_modules(file) do
+    {:ok, contents} = File.read(file)
 
     contents
     |> String.split("\n", trim: true)
     |> Enum.map(&String.to_integer/1)
   end
 
-  def large() do
-    {:ok, csv} = File.read("day02.csv")
+  def gas(module) do
+    div(module, 3) - 2
+  end
 
-    [head | _tail] =
-      csv
-      |> String.trim_trailing()
-      |> String.split(",")
-      |> Enum.map(&String.to_integer/1)
-
-    head
+  def answer_part_a() do
+    Enum.map(make_modules("resources/day01.txt"), &gas/1)
+    |> Enum.sum()
   end
 end
+
+# iex(1)> c "lib/day01.exs"
+# [Day01]
+# iex(2)> Day01.answer_part_a
+# 3337766
+
+
+
+
+
+# def large() do
+#   {:ok, csv} = File.read("day02.csv")
+
+#   [head | _tail] =
+#     csv
+#     |> String.trim_trailing()
+#     |> String.split(",")
+#     |> Enum.map(&String.to_integer/1)
+
+#   head
+# end
+# end
 
 # iex(3)> c "lib/day01.exs"
 # [Haiku]
