3ed23d4080d48f113293f8fe7bc40995bfc4d750
Eric Tobin <eatobin@gmail.com>
2023-11-05 21:39:03 -0600

    memory map

--- lib/day02/day02.exs
+++ lib/day02/day02.exs
@@ -1,11 +1,17 @@
-def large() do
-  {:ok, csv} = File.read("day02.csv")
+# c "lib/day02/day02.exs"
+# r Day02
+defmodule Day02 do
+  def make_memory_map() do
+    {:ok, csv} = File.read("lib/day02/day02.csv")
 
-  [head | _tail] =
-    csv
-    |> String.trim_trailing()
-    |> String.split(",")
-    |> Enum.map(&String.to_integer/1)
+    values =
+      csv
+      |> String.trim_trailing()
+      |> String.split(",")
+      |> Enum.map(&String.to_integer/1)
 
-  head
+    keys = Enum.to_list(0..10_000)
+
+    Enum.zip(keys, values) |> Enum.into(%{})
+  end
 end
