3343ac39dbabaa2c24998ac277731a4175a5a48d
Eric Tobin <eatobin@gmail.com>
2025-04-04 16:18:35 -0700

    add vector

--- day02/lib/day02.ex
+++ day02/lib/day02.ex
@@ -1,4 +1,6 @@
 defmodule Day02 do
+  import(PersistentVector)
+
   def make_memory_map(file) do
     {:ok, csv} = File.read(file)
 
@@ -11,6 +13,12 @@ defmodule Day02 do
     keys = Enum.to_list(0..(Enum.count(values) - 1))
 
     Enum.zip(keys, values) |> Enum.into(%{})
+  end
+
+  def my_test() do
+    v = new(1..7)
+    get(v, 0)
+    set(v, 1, :two)
   end
 
   # _memory = Day02.make_memory_map("lib/day02.csv")
